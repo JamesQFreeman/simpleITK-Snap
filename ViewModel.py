c3a75ee973aaff84a88d22afff333c00520d7247
JamesQFreeman <ws.bond1@gmail.com>
2020-07-16 20:59:03 +0800

    Re-organize the ViewModel

--- ViewModel.py
+++ ViewModel.py
@@ -13,7 +13,6 @@ class Data3D:
     # Calculations happened in View3DCore
     def __init__(self, img: ndarray):
         self.rawData = img
-        self.grayScale8 = normalizeToGrayScale8(self.rawData)
         self.shape = self.rawData.shape
         self.maxVal = np.max(self.rawData)
         self.minVal = np.min(self.rawData)
@@ -24,16 +23,17 @@ class View3D:
         sitkImg = sitk.ReadImage(imgDir)
         self.spacing = sitkImg.GetSpacing()
         self.data = Data3D(sitk.GetArrayFromImage(sitkImg))
+        self.grayScale8 = normalizeToGrayScale8(self.data.rawData)
         self.displaySize = displaySize
 
     def getXSlice(self, x: int) -> ndarray:
-        return resizeImage(self.data.grayScale8[x, :, :], self.displaySize)
+        return resizeImage(self.grayScale8[x, :, :], self.displaySize)
 
     def getYSlice(self, y: int) -> ndarray:
-        return resizeImage(self.data.grayScale8[:, y, :], self.displaySize)
+        return resizeImage(self.grayScale8[:, y, :], self.displaySize)
 
     def getZSlice(self, z: int) -> ndarray:
-        return resizeImage(self.data.grayScale8[:, :, z], self.displaySize)
+        return resizeImage(self.grayScale8[:, :, z], self.displaySize)
 
     def getHistogram(self) -> ndarray:
         fig = plt.figure(figsize=(self.displaySize[0] // 100, self.displaySize[1] // 100))
